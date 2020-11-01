a3d163f7017c50d380cfa5946ba266a4518fefcd
huanglin <934209679@qq.com>
2020-11-01 16:10:17 +0800

    完成登录

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+        <div>1234</div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        logout(){            
+            // window.sessionStorage.removeItem("token");
+            window.sessionStorage.clear("token");
+            this.$router.push("/login")
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,17 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { 
+    Button ,
+    Form,
+    FormItem,
+    Input,
+    // 弹框提示组件
+    Message
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 挂载弹框提示组件
+Vue.prototype.$message=Message
+
