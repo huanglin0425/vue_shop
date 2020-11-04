8ca714026dbe313e710aa53f8f962fc966b3d318
huanglin <934209679@qq.com>
2020-11-04 22:04:12 +0800

    git

--- src/main.js
+++ src/main.js
@@ -9,7 +9,14 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL="http://127.0.0.1:8888/api/private/v1/"
+// 通过请求拦截器添加token,保证获取数据的权限
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization=window.sessionStorage.getItem("token");
+  // 最后必须return config
+  return config
+})
 Vue.prototype.$http=axios;
+
 
 
 Vue.config.productionTip = false
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,13 +5,53 @@ import {
     FormItem,
     Input,
     // 弹框提示组件
-    Message
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog,
+    MessageBox,
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+
 // 挂载弹框提示组件
 Vue.prototype.$message=Message
+Vue.prototype.$comfirm=MessageBox.confirm
+
 
